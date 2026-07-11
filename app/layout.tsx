772a0b4687f59033c20e2f0a9e2eeae6d1596c21
sloyardms <sloyard_ms@outlook.com>
2026-07-10 23:06:08 -0700

    feat(messages): add toast notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
@@ -48,6 +49,7 @@ export default function RootLayout({
           enableColorScheme={false}
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
